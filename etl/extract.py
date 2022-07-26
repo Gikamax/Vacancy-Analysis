482b49e203d74cce93715a05e5fc4ce413e1a352
Gikamax <Pj.bolte@hotmail.com>
2022-07-26 22:20:48 +0200

    Enabled the Extraction in Docker-Compose

--- etl/extract.py
+++ etl/extract.py
@@ -1,14 +1,15 @@
 # Script to extract
 from Vacancy_Analysis import VacancyAnalysis # Import created Class
+import schedule # For scheduling the job. 
+import time
 
-# Define parameters for the extraction
-functions_list = ["Data Engineer", "Data Analist", "Business Intelligence", "Product Owner"]
-locations_list = ["Enschede", "Almelo", "Deventer", "Zwolle"]
-
-
-if __name__ == "__main__":
-    for function in functions_list: # Iterate over all functions 
-        for location in locations_list: # Then iterate over all locations 
+#Create extract_job for all the extractions
+def extract_job():
+    # Define parameters for the extraction
+    functions_list = ["Data Engineer", "Data Analist", "Business Intelligence", "Product Owner"]
+    locations_list = ["Enschede", "Almelo", "Deventer", "Zwolle"]
+    for function in functions_list:
+        for location in locations_list:
             vacancy = VacancyAnalysis(jobname=function, location=location, MongoDB_connectionstring="mongodb://localhost:27017") # Create instance of Vacancyanalis
             # Extract all elements
             vacancy.extract() # 
@@ -18,3 +19,10 @@ if __name__ == "__main__":
             vacancy.analyze()
             print(function, location)
 
+
+
+schedule.every().day.at("06:00").do(extract_job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(10800)
