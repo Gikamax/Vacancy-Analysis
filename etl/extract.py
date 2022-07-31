f048250e7befcd466e1d65dd9dbca5cb511bb00f
Gikamax <Pj.bolte@hotmail.com>
2022-07-31 12:47:47 +0200

    Changed Extract to one-time run

--- etl/extract.py
+++ etl/extract.py
@@ -1,13 +1,13 @@
 # Script to extract
 from Vacancy_Analysis import VacancyAnalysis # Import created Class
-import schedule # For scheduling the job. 
-import time
 
-#Create extract_job for all the extractions
-def extract_job():
-    # Define parameters for the extraction
-    functions_list = ["Data Engineer", "Data Analist", "Business Intelligence", "Product Owner"]
-    locations_list = ["Enschede", "Almelo", "Deventer", "Zwolle"]
+# Define functions to look for 
+functions_list = ["Data Engineer", "Data Analist", "Business Intelligence", "Product Owner"]
+# Define Locations 
+locations_list = ["Enschede", "Almelo", "Deventer", "Zwolle"]
+
+
+if __name__ == "__main__":
     for function in functions_list:
         for location in locations_list:
             vacancy = VacancyAnalysis(jobname=function, location=location, MongoDB_connectionstring="mongodb://localhost:27017") # Create instance of Vacancyanalis
@@ -18,11 +18,3 @@ def extract_job():
             # Create Analysis
             vacancy.analyze()
             print(function, location)
-
-
-extract_job()
-# schedule.every().day.at("06:00").do(extract_job)
-
-# while True:
-#     schedule.run_pending()
-#     time.sleep(10800)
